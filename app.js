405f1c984275af49b43d5e3faf86bcdaa3c7e64d
Evan Shaw <e85.shaw@gmail.com>
2022-08-10 16:48:17 -0700

    New routes with protection added, new ability to add new inputs included

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 
@@ -39,7 +40,10 @@ app.use(
     session({
         secret: 'keyboard cat',
         resave: false,
-        saveUninitialized: false
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     }))
 
 // Passport middleware
@@ -55,6 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
